c16a005e8c37ae3e1bbf25e567b190df3d88f766
HyeongJH <krack1@naver.com>
2016-08-05 21:41:52 +0900

    update

--- src/mvc/controller/findIdAction.java
+++ src/mvc/controller/findIdAction.java
@@ -1,10 +1,7 @@
 package mvc.controller;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import java.io.UnsupportedEncodingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
--- src/mvc/controller/listAction.java
+++ src/mvc/controller/listAction.java
@@ -1,9 +1,6 @@
 package mvc.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.text.SimpleDateFormat;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
